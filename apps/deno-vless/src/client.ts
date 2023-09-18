54d12e0098470b39efb90a937aa17e8a8869e58e
nci6tjq7 <112677266+nci6tjq7@users.noreply.github.com>
2023-09-19 01:47:42 +0800

    Update client.ts

--- apps/deno-vless/src/client.ts
+++ apps/deno-vless/src/client.ts
@@ -7,7 +7,7 @@ async function serveClient(req: Request, basePath: string) {
     // resp.headers.set('cache-control', 'public, max-age=2592000');
     let targetUrl = `https://raw.githubusercontent.com/nci6tjq7/edgetunnel/main/apps/cf-page-vless${url.pathname}`;
     if(url.pathname.includes(basePath)){
-      targetUrl = `https://raw.githubusercontent.com/nci6tjq7/edgetunnel/main/apps/cf-page/index.html`;
+      targetUrl = `https://raw.githubusercontent.com/nci6tjq7/edgetunnel/main/apps/cf-page-vless/index.html`;
     }
     console.log(targetUrl)
     const resp = await fetch(targetUrl);
